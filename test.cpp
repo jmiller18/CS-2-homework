2d1047d0998a699396d40a4e774f261891f0c983
jmiller18 <jmiller18@my.whitworth.edu>
2016-09-15 14:08:00 -0700

    bare skeleton

--- test.cpp
+++ test.cpp
@@ -6,6 +6,33 @@
 
 int main()
 {
+	ex01;
+	ex02;
+	ex03;
+	ex04;
+
     return 0;
 }
 
+void ex02()
+{
+	bool hasPassedTest = true;
+	int x;
+	int y;
+	int numberOfShares;
+	int book;
+	int box;
+	int shelfLife;
+}
+
+void ex03()
+{
+}
+
+void ex04()
+{
+}
+
+void ex05()
+{
+}
